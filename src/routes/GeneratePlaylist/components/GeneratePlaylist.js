13c2aa298f1f92d6f51d0612c9afa6c513d8a495
Stephanie Liu <sliu@magento.com>
2017-01-29 03:42:26 -0500

    essential copy changes

--- src/routes/GeneratePlaylist/components/GeneratePlaylist.js
+++ src/routes/GeneratePlaylist/components/GeneratePlaylist.js
@@ -8,7 +8,7 @@ export default class GeneratePlaylistView extends Component {
     return (
       <div className="home">
         <div className="home__container text--center">
-          <h1 className="text--green">Fryin' up some corn dogs</h1>
+          <h1 className="text--green">Fryin' up some corn, dawgz</h1>
           <img src='https://media.giphy.com/media/3oEjHGIMl0qrDHamqs/giphy.gif' alt='korn'/>
         </div>
       </div>
