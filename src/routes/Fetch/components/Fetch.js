8d016a89c11c2ec2a6964e4e2ca2a56aad7a8486
Stephanie Liu <sliu@magento.com>
2017-01-28 20:01:49 -0500

    redirect on finish

--- src/routes/Fetch/components/Fetch.js
+++ src/routes/Fetch/components/Fetch.js
@@ -9,11 +9,17 @@ export default class FetchView extends Component {
   }
 
   render() {
+    const { redirectToFeatures } = this.props;
     return (
       <div className="home">
         <div className="home__container text--center">
           <h1 className="text--green">Corndog Playlist Generator</h1>
           <h2 className="text--white">Generate the playlist of your life by customizing settings and all the junk! All you have to do is log in to your Spotify account.</h2>
+          <Button
+            onClick={redirectToFeatures}
+            className="cd-button--green"
+            buttonName="Log in to Spotify"
+          />
         </div>
       </div>
     )
--- src/routes/Fetch/modules/fetch.js
+++ src/routes/Fetch/modules/fetch.js
@@ -1,16 +1,15 @@
+import { browserHistory } from 'react-router'
 // ------------------------------------
 // Constants
 // ------------------------------------
-export const COUNTER_INCREMENT = 'COUNTER_INCREMENT'
-export const COUNTER_DOUBLE_ASYNC = 'COUNTER_DOUBLE_ASYNC'
 export const FETCH_API_CALLED = 'FETCH_API_CALLED'
 export const FETCH = 'FETCH_MATRIX_STORED'
+export const REDIRECT_TO_FEATURES = 'REDIRECT_TO_FEATURES'
 
 // ------------------------------------
 // Actions
 // ------------------------------------
-export function calledApi(){
-  console.log('calledApi');
+export function calledApi() {
   const value = ['I','CAN','MAKE','THIS','WORK'];
   return {
     type: FETCH_API_CALLED,
@@ -18,54 +17,34 @@ export function calledApi(){
   }
 }
 
-export function increment (value = 1) {
-  // Hit SP API
-
-  // Return 
+export function redirectToFeatures() {
+  browserHistory.push('/')
   return {
-    type    : COUNTER_INCREMENT,
-    payload : value
-  }
-}
-
-/*  This is a thunk, meaning it is a function that immediately
-    returns a function for lazy evaluation. It is incredibly useful for
-    creating async actions, especially when combined with redux-thunk! */
-
-export const doubleAsync = () => {
-  return (dispatch, getState) => {
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        dispatch({
-          type    : COUNTER_DOUBLE_ASYNC,
-          payload : getState().fetch
-        })
-        resolve()
-      }, 200)
-    })
+    type: REDIRECT_TO_FEATURES
   }
 }
 
 export const actions = {
-  increment,
-  doubleAsync,
-  calledApi
+  calledApi,
+  redirectToFeatures
 }
 
 // ------------------------------------
 // Action Handlers
 // ------------------------------------
 const ACTION_HANDLERS = {
-  [COUNTER_INCREMENT]    : (state, action) => state + action.payload,
-  [COUNTER_DOUBLE_ASYNC] : (state, action) => state * 2,
-  [FETCH_API_CALLED] : (state, action) => { return {...state, songList:action.payload}}
+  [FETCH_API_CALLED] : (state, action) => { 
+    return {
+      ...state,
+      songList:action.payload
+    }
+  }
 }
 
 // ------------------------------------
 // Reducer
 // ------------------------------------
 const initialState = {
-  somethingElse: [],
   songList: ['OMG']
 }
 export default function fetchReducer (state = initialState, action) {
